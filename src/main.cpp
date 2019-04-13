dd8790538ce76d1d9bbe3c950fd0ba2bc0f0839b
Gabriel Martins <ga.smartins94@gmail.com>
2019-04-13 15:43:08 -0300

    Leitura de Oxigenacao e Frequencia Cardiaca

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ uint32_t tsLastReport = 0;
 void setupOLEDDisplay();
 void setupMAX30100();
 void displayHeartRate();
+void displaySPO2();
 
 void setup() {
   Serial.begin(115200);
@@ -23,14 +24,14 @@ void setup() {
 }
 
 void setupOLEDDisplay(){
-   Serial.println("Initializing Health Reader ...");
+   Serial.println("Initializing Health Reader ...\n");
    display.init();
    display.flipScreenVertically();
    display.setFont(ArialMT_Plain_16);
    display.setTextAlignment(TEXT_ALIGN_LEFT);
    display.drawStringMaxWidth(0, 0, 128, "Initializing Health Reader ..." );
    display.display();
-   delay(100);
+   delay(10000);
 }
 
 void onBeatDetected()
@@ -40,29 +41,32 @@ void onBeatDetected()
 
 void setupMAX30100(){
   display.clear();
-  Serial.print("Initializing pulse oximeter...");
+  Serial.print("Initializing pulse oximeter...\n");
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawStringMaxWidth(0, 0, 128,"Initializing pulse oximeter...\n" );
+  display.drawStringMaxWidth(0, 0, 128,"Initializing pulse oximeter..." );
   display.display();
-  delay(100);
+  delay(1000);
 
-  display.clear();
-  if (!pox.begin()){
-    Serial.println("FAILED");
-    display.setTextAlignment(TEXT_ALIGN_CENTER);
-    display.drawString(64, 22, "Failed ... ");
-  for(;;);
-  } else {
-    Serial.println("SUCCESS");
-     display.setTextAlignment(TEXT_ALIGN_CENTER);
-    display.drawString(64, 22, "Success ... ");
-  }
-  display.display();
-   delay(100);
-  pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+  pox.begin();
+  pox.setIRLedCurrent(MAX30100_LED_CURR_17_4MA);
   pox.setOnBeatDetectedCallback(onBeatDetected);
 
+  display.clear();
+
+  while(!pox.begin()){
+    Serial.println("FAILED");
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.clear();
+    display.drawStringMaxWidth(0, 0, 128,"Trying to intilize MAX30100 sensor ..." );
+    display.drawStringMaxWidth(0, 32, 128,"Failed ..." );
+    display.display();
+  }
+
+  Serial.println("SUCCESS");
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.drawString(0, 2, "Success ... ");
+  display.display();  
 }
 
 
@@ -73,27 +77,33 @@ void readMAX30100(){
   pox.update();
   
   if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-  Serial.print("Heart rate:");
-  Serial.print(pox.getHeartRate());
-  displayHeartRate();
-  Serial.print("bpm / SpO2:");
-  Serial.print(pox.getSpO2());
-  Serial.println("%");
-  
-  tsLastReport = millis();
+    display.clear();
+    displayHeartRate();
+    displaySPO2();
+    tsLastReport = millis();
   }
 }
 
 void displayHeartRate(){
-   display.clear();
+   Serial.print("Heart rate:\n");
+   Serial.print(pox.getHeartRate());
    display.setFont(ArialMT_Plain_10);
    display.setTextAlignment(TEXT_ALIGN_CENTER);
-   display.drawStringMaxWidth(0, 0, 128,"Heart rate: ..." );
+   display.drawString(0, 0,"BPM : ..." + String(pox.getHeartRate()));
+   display.display();
+}
+
+void displaySPO2(){
+   Serial.print("BPM / SpO2: \n");
+   Serial.print(String(pox.getSpO2()) + "% \n");
+   display.setFont(ArialMT_Plain_10);
+   display.setTextAlignment(TEXT_ALIGN_CENTER);
+   display.drawString(0, 32,"SP02 : ..." + String(pox.getSpO2()) + "%");
    display.display();
 }
 
 
 
 void loop() {
-  //readMAX30100();
+    readMAX30100();
 }
